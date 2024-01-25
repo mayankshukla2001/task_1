a0147e36ff6427c32d2012cd27a2601524011241
Abhishek Yadav <124059750+Abhishek-YadavSD@users.noreply.github.com>
2024-01-25 19:19:11 +0530

    user data is appended in step-1

--- ind.js
+++ ind.js
@@ -20,15 +20,50 @@ function subuser(){
     userdata.dob=document.querySelector("#dob").value;
     userdata.gender=document.querySelector("#gender").value;
 
-    document.querySelector("#lp3").style.display='';
+    // document.querySelector("#lp3").style.display='';
     document.querySelector("#lph").style.display="none";
     document.querySelector("#btn-final").style.display=''
 
-    document.querySelector(`#test-user`).innerHTML=userdata.name;
-    document.querySelector(`#test-numb`).innerHTML=userdata.mnumber;
-    document.querySelector(`#test-gender`).innerHTML=userdata.gender;
-    document.querySelector(`#test-dob`).innerHTML=userdata.dob;
-    i++
+    document.querySelector("#usersave").classList.remove('d-none');
+
+    
+ 
+
+    
+    let xcont=`<div id="lp3">
+    <div class="lower-part-hidden justify-content-between user-data">
+       <span class="d-flex justify-content-arround">
+       <input type="checkbox" name="" id="checkedpt">
+       <div class="test-text">
+           <h2 id="test-user" class="title">${userdata.name}</h2>
+           <h3 id="test-numb">${userdata.mnumber}</h3>
+           <h3 id="test-gender" class="secondary-text">${userdata.gender}</h3>
+           <h3 id="test-dob" class="secondary-text">${userdata.dob}</h3>
+       </div>
+       </span>
+       <span class="d-flex justify-content-around">
+       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+           <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+           <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+         </svg>
+       <svg onclick="dlt()" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FF2B2B" class="bi bi-trash" viewBox="0 0 16 16">
+           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+           <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+         </svg>
+       </span>
+                         
+    </div>
+
+   </div>`
+   let rn= document.querySelector("#ustext");
+   rn.insertAdjacentHTML("afterbegin",xcont);
+
+
+    // document.querySelector(`#test-user`).innerHTML=userdata.name;
+    // document.querySelector(`#test-numb`).innerHTML=userdata.mnumber;
+    // document.querySelector(`#test-gender`).innerHTML=userdata.gender;
+    // document.querySelector(`#test-dob`).innerHTML=userdata.dob;
+
 }
 // document.querySelector("#btn-final").addEventListener('click' , function (){console.log("it is final buttok")})
 function step2(){
@@ -173,4 +208,8 @@ function addaddress(){
 
 
    
+}
+function dlt(){
+    alert("working dlt")
+    this.document.querySelector(".user-data").remove()
 }
